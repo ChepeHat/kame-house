8540321612cb6ab601fa8af9b76f8562529161a1
ChepeHat <josesombrerero@hotmail.com>
2024-05-21 01:21:47 -0600

    API Fake de Vercel vinculada

--- js/services/product-services.js
+++ js/services/product-services.js
@@ -1,11 +1,11 @@
 const productList = () => {
-    return fetch("http://localhost:3001/products")
+    return fetch("https://fake-api-topaz.vercel.app/products")
             .then((response) => response.json())
             .catch ((error) => console.log(error))
 }
 
 const createProduct = (name, price, image) => {
-    return fetch("http://localhost:3001/products", {
+    return fetch("https://fake-api-topaz.vercel.app/products", {
     method: "POST",
     headers: {
         "Content-Type": "application/json",
@@ -20,7 +20,7 @@ const createProduct = (name, price, image) => {
 };
 
 const deleteProduct = (id)  => {
-    return fetch(`http://localhost:3001/products/${id}`, {
+    return fetch(`https://fake-api-topaz.vercel.app/products/${id}`, {
     method: "DELETE",
     headers: {
         "Content-Type": "application/json",
